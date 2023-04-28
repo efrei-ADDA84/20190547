20b6f8c2825d03e96094a2177e210ca59ca3a56d
Bardane <theo.martin@efrei.net>
2023-04-28 11:24:30 +0200

    added workflow and api

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
 FROM python:3.8
 
+
+
 WORKDIR /app
 
-COPY weather.py .
+
+
+COPY main.py .
+
 
 RUN pip install requests
+RUN pip install fastapi
+RUN pip install uvicorn
 
-CMD [ "python", "main.py" ]
+
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8081"]
--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import requests
 import os
+from fastapi import FastAPI
 
-def get_weather(lat, lon, api_key):
+
+app = FastAPI()
+api_key = os.environ.get("API_KEY")
+
+@app.get("/")
+def get_weather(lat, lon):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
     response = requests.get(url)
     data = response.json()
     return data
-
-lat = os.environ.get("LAT")
-lon = os.environ.get("LONG")
-
-api_key = os.environ.get("API_KEY")
-
-weather_data = get_weather(lat, lon, api_key)
-
-print(weather_data)
-
-
-temperature = weather_data["main"]["temp"]
-endroit = weather_data["name"]
-meteo = weather_data["weather"][0]["description"]
-
-print(f"La température actuelle est de {temperature} degrés Celsius à {endroit}.")
-print(f"Le temps est {meteo}.")
